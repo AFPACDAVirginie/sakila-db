c49b6f02150a3032ebe8272a560ee528baff9bba
Virginie16 <63580406+Virginie16@users.noreply.github.com>
2024-12-06 11:01:24 +0100

    reste les procédures à faire sur le 6

--- todo/05-views-creation.sql
+++ todo/05-views-creation.sql
@@ -15,10 +15,21 @@
 -->
 CREATE OR REPLACE
 VIEW `long_films` AS
-??????
+    SELECT film.title,film.length,film.language_id, `language`.`name`
+    FROM `film` 
+    INNER JOIN `language` ON film.language_id=`language`.language_id
+    WHERE length>120  
+    ORDER BY `film`.`length` ASC;
 
 -- Créer une vue "customers_with_rentals" qui présente tous les client qui ont des prêts en cours.
 --> 
+CREATE VIEW `customers_with_rentals` AS
+SELECT DISTINCT rental.customer_id, customer.last_name, customer.first_name FROM `rental`  
+INNER JOIN customer ON rental.customer_id=customer.customer_id
+WHERE return_date IS NULL  
+ORDER BY `rental`.`customer_id` ASC;
+
+
 
 -- Créer une vue "film_list" qui présente toutes les informations des films (ormis la langue) avec, en plus les noms des acteurs (sous forme de chaîne de caractères dans une colonne)
 -- Indices :
@@ -27,11 +38,34 @@ VIEW `long_films` AS
 CREATE OR REPLACE
 VIEW `film_list` AS
 select
-    `film`.`film_id` AS `id`,
-    ?????
-    group_concat(???????????) AS `actors`
+    `film`.`film_id` AS `FID`,
+    `film`.`title` AS `title`,
+    `film`.`description` AS `description`,
+    `category`.`name` AS `category`,
+    `film`.`rental_rate` AS `price`,
+    `film`.`length` AS `length`,
+    `film`.`rating` AS `rating`,
+    GROUP_CONCAT(
+        CONCAT(
+            `actor`.`first_name`,
+            ' ',
+            `actor`.`last_name`
+        ) SEPARATOR ', '
+    ) AS `actors`
 from
-    ?????? JOINTURE ?????
+    ((((`film`
+                LEFT JOIN `film_category` ON
+                    (`film_category`.`film_id` = `film`.`film_id`
+                    ))
+            LEFT JOIN `category` ON
+                (`category`.`category_id` = `film_category`.`category_id`
+                ))
+        LEFT JOIN `film_actor` ON
+            (`film`.`film_id` = `film_actor`.`film_id`
+            ))
+    LEFT JOIN `actor` ON
+        (`film_actor`.`actor_id` = `actor`.`actor_id`
+        ))
 group by
     `film`.`film_id`,
     `category`.`name`;
@@ -41,3 +75,26 @@ group by
 -- + procédez en 2 étapes : 1->conception de la requête qui sélectionne les 3 genres avec leur revenu respectif, 2->construction de la vue
 -- + pour la requête, vous allez utiliser les tables "category", "film_category", "inventory", "payment" et "rental"
 --> 
+SELECT category.name AS `categorie`, SUM(payment.amount) AS `total_ventes`
+FROM (((((`payment`
+          JOIN `rental` ON (`payment`.`rental_id` = `rental`.`rental_id`)
+                    )
+                JOIN `inventory` ON (`rental`.`inventory_id` = `inventory`.`inventory_id`)
+                )
+            JOIN `film` ON (`inventory`.`film_id` = `film`.`film_id`)
+            )
+        JOIN `film_category` ON (`film`.`film_id` = `film_category`.`film_id`)
+        )
+    JOIN `category` ON (`film_category`.`category_id` = `category`.`category_id`)
+    )
+GROUP BY `category`.`name`
+ORDER BY SUM(`payment`.`amount`) DESC
+LIMIT 3
+
+-- exemple de la vue `actor_info`
+-- select `a`.`actor_id` AS `actor_id`,`a`.`first_name` AS `first_name`,`a`.`last_name` AS `last_name`,group_concat(distinct concat(`c`.`name`,': ',(select group_concat(`f`.`title` order by `f`.`title` ASC separator ', ') 
+-- from ((`sakila`.`film` `f` join `sakila`.`film_category` `fc` on(`f`.`film_id` = `fc`.`film_id`)) join `sakila`.`film_actor` `fa` on(`f`.`film_id` = `fa`.`film_id`)) 
+-- where `fc`.`category_id` = `c`.`category_id` and `fa`.`actor_id` = `a`.`actor_id`)) 
+-- order by `c`.`name` ASC separator '; ') AS `film_info` 
+-- from (((`sakila`.`actor` `a` left join `sakila`.`film_actor` `fa` on(`a`.`actor_id` = `fa`.`actor_id`)) left join `sakila`.`film_category` `fc` on(`fa`.`film_id` = `fc`.`film_id`)) 
+-- left join `sakila`.`category` `c` on(`fc`.`category_id` = `c`.`category_id`)) group by `a`.`actor_id`,`a`.`first_name`,`a`.`last_name`
--- todo/06-procedures.sql
+++ todo/06-procedures.sql
@@ -19,16 +19,50 @@
 -- + int, identifiant de l'utilisateur
 -- Donnée en sortie :
 -- + VARCHAR(20), le status de l'utilisateur
+CREATE FUNCTION sakila.user_status(IN user_id INT)
+RETURNS VARCHAR(20)
+BEGIN
+    DECLARE rentals_count INT DEFAULT 0;
+    DECLARE user_status VARCHAR(20);
+
+    -- Compter le nombre de locations pour l'utilisateur
+    SELECT COUNT(*) INTO rentals_count FROM rental WHERE customer_id = user_id;
+
+    -- Déterminer le statut utilisateur en fonction du nombre de locations
+    IF rentals_count <= 10 THEN
+        SET user_status = 'Casual viewer';
+    ELSEIF rentals_count <= 30 THEN
+        SET user_status = 'Amateur';
+    ELSE
+        SET user_status = 'Cinéphile';
+    END IF;
+
+    -- Retourner le statut de l'utilisateur
+    RETURN user_status;
+END;
+
+-- utilisation :
+SELECT 
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    sakila.user_status(c.customer_id) AS status
+FROM customer c  
+ORDER BY `c`.`customer_id` ASC;
+
 CREATE FUNCTION sakila.user_status(IN user_id int)
 RETURNS VARCHAR(20)
 BEGIN
 	DECLARE rentals_count INT DEFAULT 0;
 
 	SELECT count(*) INTO rentals_count FROM rental WHERE customer_id = user_id;
-
+RETURN user_status;
 	-- Implémenter une structure de contrôle permettant de renvoyer la chaîne de caractère attendue
     -- Plus d'information sur les structures de contrôle en MariaDB : https://mariadb.com/kb/en/programmatic-compound-statements/
 END
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
 
 -- Ecrire une fonction qui retourne le nombre de DVD disponibles à location (non empruntés) proposés par un magasin.
 -- 
@@ -37,6 +71,7 @@ END
 --
 -- Donnée d'entrée :
 -- + int, identifiant d'un magasin
+DELIMITER $$
 CREATE FUNCTION sakila.count_inventory(IN store_id int)
 RETURNS INT
 BEGIN
@@ -47,14 +82,32 @@ BEGIN
 	SET store_ok = EXISTS(SELECT * from store s WHERE s.store_id = store_id);
 	
 	-- Traiter l'erreur au besoin
-    -- ??????????
+    IF store_ok = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Magasin non trouvé';
+    END IF;
 
-	SELECT count(*) into ????????? FROM inventory i WHERE i.store_id = 1
-	AND i.inventory_id NOT IN (SELECT r.inventory_id FROM rental r WHERE r.return_date IS ?????);
+	-- Comptage des DVDs disponibles
+    SELECT COUNT(*) INTO inventory_count
+    FROM inventory i
+    WHERE i.store_id = store_id
+    AND i.inventory_id NOT IN (
+        SELECT r.inventory_id 
+        FROM rental r 
+        WHERE r.return_date IS NULL
+    );
 	
 	return inventory_count;
-END
+END$
 
+DELIMITER ;
+
+--utilisation : avec store_id=2, retourne 2221
+SET @p0='2'; SELECT `count_inventory_film`(@p0) AS `count_inventory_film`;
+
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
 
 -- Ecrire une fonction qui renvoie "VRAI" si le DVD est en inventaire dans un magasin précisé en paramètre.
 -- 
@@ -68,16 +121,31 @@ END
 --
 -- Pour tester votre fonction utilisez l'appel suivant : SELECT is_film_in_inventory(1, "ACADEMY DINOSAUR");
 CREATE FUNCTION sakila.is_film_in_inventory(int store_id, TEXT film_title)
-RETURNS ????????
+RETURNS TINYINT(1)
 BEGIN
     -- Déclaration de la variable résultat
     DECLARE inventory_count INT DEFAULT 0;
 
     -- Faire la requête permettant de mettre à jour la variable
-    -- Pour plus d'informations concernant les variables MySQL : https://www.mysqltutorial.org/mysql-stored-procedure/variables-in-stored-procedures/
+    SELECT COUNT(*) INTO inventory_count
+    FROM inventory i
+    JOIN film f ON i.film_id = f.film_id
+    WHERE i.store_id = store_id
+      AND f.title = film_title;
 
     -- retourner le résultat de la fonction
+     IF inventory_count > 0 THEN
+        RETURN 1;
+    ELSE
+        RETURN 0;
+    END IF;
 END
+
+-- utilisation => renvoie 1
+SET @p0='1'; SET @p1='ACADEMY DINOSAUR'; SELECT `is_film_in_inventory`(@p0, @p1) AS `is_film_in_inventory`;
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
 
 --
 -- Ecrire une procédure qui renvoie une chaîne de caractère correspondant à la concaténation des 3 films les plus loués.
@@ -89,21 +157,95 @@ END
 -- Avec le jeu d'essai founi vous devriez obtenir : "FORWARD TEMPLE, ROCKETEER MOTHER, BUCKET BROTHERHOOD"
 --
 -- Ci-dessous la déclaration de la procédure et des indices sur le développement
-CREATE FUNCTION sakila.most_rented_films()
-RETURNS TEXT 
+
+DELIMITER $$ 
+CREATE PROCEDURE sakila.most_rented_films(OUT result TEXT)
 BEGIN
     -- Vous allez utiliser un curseur pour passer en revue les résultats d'une requête
-    -- Voici un tutoriel sur le fonctionnement des curseurs en MySQL : https://waytolearnx.com/2019/11/les-curseurs-dans-mysql.html
+    
 
     -- Les étapes de l'algorithme à développer sont :
     -- 1 déclaration des variables qui seront utilisées
-
+    DECLARE done INT DEFAULT 0;
+    DECLARE film_title TEXT;
+    SET result = '';
+    -- DECLARE result TEXT DEFAULT '';
     -- 2 Déclaration du curseur avec la requête répondant au besoin (vous avez peut être déjà fait cette requête :) )
-
+    DECLARE cur CURSOR FOR 
+        SELECT f.title
+        FROM film f
+        JOIN inventory i ON f.film_id = i.film_id
+        JOIN rental r ON i.inventory_id = r.inventory_id
+        GROUP BY f.title
+        ORDER BY COUNT(r.rental_id) DESC
+        LIMIT 3;
     -- 3 Boucle qui permettra de passer en revue les résultat de la requête et constuire la chaîne de caractère résultat
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+    OPEN cur;
+    read_loop: LOOP
+        FETCH cur INTO film_title;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
 
     -- 4 retour de la chaîne de caractères résultat
-end
+     IF result = '' THEN
+            SET result = film_title; -- Pas de virgule pour le premier titre
+        ELSE
+            SET result = CONCAT(result, ', ', film_title); -- Ajout de la virgule pour les suivants
+        END IF;
+    -- SET result = CONCAT(result, film_title);
+    -- IF done = 0 THEN
+    --         SET result = CONCAT(result, ', ');
+    --     END IF;
+    END LOOP;
+    CLOSE cur;
+    
+END $$
+
+DELIMITER ;
+
+DELIMITER
+    $$
+CREATE PROCEDURE sakila.most_rented_films_1(OUT result TEXT)
+BEGIN
+    DECLARE
+        done INT DEFAULT 0 ; DECLARE film_title TEXT ; DECLARE result TEXT DEFAULT '' ; 
+    DECLARE cur CURSOR FOR
+    SELECT
+        f.title
+    FROM
+        film f
+    JOIN inventory i ON
+        f.film_id = i.film_id
+    JOIN rental r ON
+        i.inventory_id = r.inventory_id
+    GROUP BY
+        f.title
+    ORDER BY
+        COUNT(r.rental_id)
+    DESC
+LIMIT 3 ; 
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1 ; 
+OPEN cur ; 
+read_loop: LOOP 
+	FETCH cur INTO film_title ; 
+    IF done THEN LEAVE read_loop ;
+	END IF ; 
+    
+    IF result IS NOT NULL AND result != '' THEN
+            SET result = CONCAT(result, ', ');
+        END IF;
+
+        
+        SET result = CONCAT(result, film_title);
+END LOOP ; 
+CLOSE cur ;
+END$
+
+DELIMITER ;
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
 
 
 -- Ecrire une procédure qui renvoie la médiane de la durée des films
